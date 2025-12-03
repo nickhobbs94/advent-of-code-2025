8d0e416a135b9f83ead7cb9702617151b1a1236b
Nicholas Hobbs <nick@nhobb.com>
2025-12-03 17:05:47 +1100

    3-2

--- 3/lib.ts
+++ 3/lib.ts
@@ -1,26 +1,33 @@
 
-function maxJolts(bank: string, best: number, remaining: number, prefix: string) {
-  if (remaining === 0) return parseInt(prefix);
-
-  for (let i=0; i<=bank.length-remaining; i++) {
-    let newprefix = prefix + bank[i];
-    let rbank = bank.slice(i+1);
-    let rbest = maxJolts(rbank, best, remaining-1, newprefix);
-    if (rbest > best) best = rbest;
+function maxJolts(bank: string) {
+  let best = 0;
+  let remaining = 12;
+  let prefix = '';
+  let n = 9;
+  while (n>0 && remaining > 0) {
+    let next = bank.indexOf(`${n}`);
+    if (next<0 || next+remaining > bank.length) {
+      n--;
+    } else {
+      bank = bank.slice(next+1);
+      prefix = `${prefix}${n}`;
+      remaining--;
+      n = 9;
+    }
   }
-
-  return best;
+  if (n===0 && remaining > 0) throw new Error(`Failed ${n}, ${remaining}, ${bank}, ${prefix}`);
+  const result = parseInt(prefix);
+  return result;
 }
 
 
 export function main(data: string) {
-  const banks = data.split('\n');
+  const banks = data.split('\n').filter(line => !!line);
 
   let sum = 0;
   let count = 0;
   for (let bank of banks) {
-    console.log(count++, banks.length);
-    sum += maxJolts(bank, 0, 12, '');
+    sum += maxJolts(bank);
   }
   return sum;
 }
